e0ce0b413ed3b227ef3a99fb88c86c26a24f909f
VENOUF <venouf8@gmail.com>
2018-05-31 10:06:35 +0200

    7eme commit "admin"

--- admin/crud/jouets/update.php
+++ admin/crud/jouets/update.php
@@ -2,30 +2,30 @@
 require_once '../../../model/database.php';
 
 $id = $_GET["id"];
-$user = getOneEntity("user", $id);
+$toy = getOneEntity("toy", $id);
 
 require_once '../../layout/header.php';
 ?>
 
-<h1>Modifier user</h1>
+<h1>Modifier un Jouet</h1>
 
 <form action="update_query.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label for="lastname">Nom</label>
-        <input type="text" id="lastname" name="lastname" value="<?php echo $user["lastname"]; ?>" class="form-control">
+        <label for="title">Nom</label>
+        <input type="text" id="title" name="title" value="<?php echo $toy["title"]; ?>" class="form-control">
     </div>
     <div class="form-group">
-        <label for="firstname">Prénom</label>
-        <input type="text" id="firstname" name="firstname" value="<?php echo $user["firstname"]; ?>" class="form-control">
+        <label for="description">Description</label>
+        <input type="text" id="description" name="description" value="<?php echo $toy["description"]; ?>" class="form-control">
     </div>
     <div class="form-group">
         <label for="picture">Photo</label>
         <input type="file" id="picture" name="picture" accept="image/*" class="form-control">
-        <?php if (!empty($user["picture"])) : ?>
-            <img src="../../../uploads/<?php echo $user["picture"]; ?>" class="img-thumbnail">
+        <?php if (!empty($title["picture"])) : ?>
+            <img src="../../../uploads/<?php echo $toy["picture"]; ?>" class="img-thumbnail">
         <?php endif; ?>
     </div>
-    <input type="hidden" name="id" value="<?php echo $user["id"]; ?>">
+    <input type="hidden" name="id" value="<?php echo $toy["id"]; ?>">
     <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Enregistrer</button>
 </form>
 
